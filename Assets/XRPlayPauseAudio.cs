c18e976b5e486730601e4383f2deccad27a610dc
daltonbo <daltonbo@uw.edu>
2024-11-09 10:00:04 -0800

    Slider for controlling volume

--- Assets/XRPlayPauseAudio.cs
+++ Assets/XRPlayPauseAudio.cs
@@ -1,11 +1,17 @@
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Content.Interaction;
 
 public class XRPlayPauseAudio : MonoBehaviour
 {
     public AudioSource audioSource; // Attach your AudioSource in the Inspector
+    public XRSlider slider;
     private bool isPlaying = false;
 
+    public void Update()
+    {
+        audioSource.volume = slider.value;
+    }
 
     // This method will be called on Select Entered
     public void OnSelectEntered(SelectEnterEventArgs args)
