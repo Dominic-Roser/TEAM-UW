1bcdf65908286fd7d3b8dab9dd68cf8fb15d6fdc
daltonbo <daltonbo@uw.edu>
2024-11-10 06:46:27 -0800

    Sort of fixed color buttons

---
+++ Assets/scri.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ColorSelectButton : MonoBehaviour
+{
+    public GameObject targetObject; // Drag and drop "FFT - Array Drums" here
+    private FFTObjectArray_v3 fftScript; // Reference to the FFTObjectArray_v3 script
+    public Color newEmissionColor; // Color to set when button is pressed
+
+    private bool isPressed = false;
+
+    private void Start()
+    {
+
+        fftScript = targetObject.GetComponent<FFTObjectArray_v3>();
+    }
+
+    // This method will be called on Select Entered
+    public void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        // Set the emission color to the new color
+        fftScript._EmissionCol = newEmissionColor;
+    }
+}
