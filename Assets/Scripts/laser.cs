ba9c1ba199f8315da0e31bce06c23148d465812b
Sean0z <seanozinsky@gmail.com>
2024-11-10 06:42:22 -0800

    laser show wooooo

--- Assets/Scripts/laser.cs
+++ Assets/Scripts/laser.cs
@@ -12,13 +12,13 @@ public class laser : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        lr.SetPosition(0, transform.position);
+        /*lr.SetPosition(0, transform.position);
         RaycastHit hit;
         if( Physics.Raycast(transform.position, transform.forward, out hit)){
             if (hit.collider) { 
                 lr.SetPosition(1, hit.point);
             }
         }
-        else lr.SetPosition(1, transform.forward*5000);
+        else lr.SetPosition(1, transform.forward*5000);*/
     }
 }
